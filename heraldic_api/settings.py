1fab303045b04b27f9cf4008c80c32df4f8e7bec
loudy-art <naza.avino@gmail.com>
2023-03-08 17:17:06 -0300

    dios me libre

--- heraldic_api/settings.py
+++ heraldic_api/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.getenv("heraldiclibrary.com,104.16.244.78,127.0.0.1,localhost").split(",")
+#ALLOWED_HOSTS = os.getenv("104.16.244.78", "127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = os.getenv("104.16.244.78", "heraldiclibrary.com", "127.0.0.1", "localhost")
 DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 # Application definition
 
